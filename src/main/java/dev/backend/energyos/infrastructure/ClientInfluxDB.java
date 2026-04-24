ee0664128267d54b04a39815e6bbd417f76df250
iago longen <iago.mendonca@martinellitech.com.br>
2026-04-23 23:10:08 -0300

    rapaz, foi dificil, mas consegui com muito esforço e tudo atualizado.

--- src/main/java/dev/backend/energyos/infrastructure/ClientInfluxDB.java
+++ src/main/java/dev/backend/energyos/infrastructure/ClientInfluxDB.java
@@ -3,12 +3,25 @@ package dev.backend.energyos.infrastructure;
 import com.influxdb.v3.client.InfluxDBClient;
 
 public final class ClientInfluxDB {
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
+
         String hostUrl = "https://us-east-1-1.aws.cloud2.influxdata.com";
-        char[] authToken = System.getenv("INFLUXDB_TOKEN").toCharArray();
+
+        String token = System.getenv("INFLUXDB_TOKEN");
+
+        if (token == null) {
+            throw new RuntimeException("INFLUXDB_TOKEN não encontrado nas variáveis de ambiente");
+        }
+
+        char[] authToken = token.toCharArray();
 
         try (InfluxDBClient client = InfluxDBClient.getInstance(hostUrl, authToken, null)) {
 
+            System.out.println("✅ Conectado com sucesso ao InfluxDB!");
+
+        } catch (Exception e) {
+            System.out.println("❌ Erro ao conectar:");
+            e.printStackTrace();
         }
     }
 }
